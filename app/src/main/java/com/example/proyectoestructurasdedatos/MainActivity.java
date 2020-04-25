8fcf3be83559effd1864bcbea97a2c322c8b2783
David Bricestone <29939205+jdbu2002@users.noreply.github.com>
2020-04-25 17:09:59 -0500

    p

--- app/src/main/java/com/example/proyectoestructurasdedatos/MainActivity.java
+++ app/src/main/java/com/example/proyectoestructurasdedatos/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.proyectoestructurasdedatos;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.*;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
