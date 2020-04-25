0bb7b71d5704f91d59f919f05d42526a9a5ae2c5
David Bricestone <29939205+jdbu2002@users.noreply.github.com>
2020-04-25 16:10:15 -0500

    LinkRegistro

--- app/src/main/java/com/example/proyectoestructurasdedatos/MainActivity.java
+++ app/src/main/java/com/example/proyectoestructurasdedatos/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.proyectoestructurasdedatos;
 
+import android.content.Intent;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView registerLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerLink = (TextView) findViewById(R.id.tv_Registrese);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegistroDatos.class));
+            }
+        });
+
     }
 
     public void iniciarSesion( View view ) {
 
     }
 
-    public void irVentanaRegistro ( View view ) {
-
-    }
 }
---
+++ app/src/main/java/com/example/proyectoestructurasdedatos/RegistroDatos.java
@@ -0,0 +1,14 @@
+package com.example.proyectoestructurasdedatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_datos);
+    }
+}
