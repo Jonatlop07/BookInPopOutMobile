9deab0b7ce591570943e71689798df69520234d9
Jonathan Lopez <jonatlop@gmail.com>
2020-04-26 20:40:08 -0500

    Había olvidado actualizar el script de registro

--- app/src/main/java/com/example/proyectoestructurasdedatos/scripts_conexion/registrarUsuario.php
+++ app/src/main/java/com/example/proyectoestructurasdedatos/scripts_conexion/registrarUsuario.php
@@ -2,9 +2,16 @@
 
 include 'conexion.php';
 
-$direccion = $_POST['DIRECCION'];
+$id_usuario = $_POST['id_usuario'];
+$id_documento = $_POST['id_documento'];
+$nombres = $_POST['nombre'];
+$apellidos = $_POST['apellido'];
+$fecha_nacimiento = $_POST['fecha_nacimiento'];
+$correo = $_POST['correo'];
+$discapacitado = $_POST['discapacitado'];
 
-$insercion = "insert into direcciones(direccion) VALUES ('" . $direccion . "')";
+$insercion = "insert into usuario values (" . $id_usuario . ", 1, " . $id_documento . ", '" . $nombres . "', '" . $apellidos . "', '" . $fecha_nacimiento . "', '" . $correo . "', " . $discapacitado . ")";
+
 
 mysqli_query($conexion, $insercion) or die(mysqli_error($conexion));
 mysqli_close($conexion);
