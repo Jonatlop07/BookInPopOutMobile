e5bf4c4e0814241dd70cbb68a236721bcdbd9c96
David Bricestone <29939205+jdbu2002@users.noreply.github.com>
2020-07-14 21:43:41 -0500

    Añadido Desencolar a un Usuario Específico

--- app/src/main/java/com/example/proyectoestructurasdedatos/listAdapter.java
+++ app/src/main/java/com/example/proyectoestructurasdedatos/listAdapter.java
@@ -1,13 +1,17 @@
 package com.example.proyectoestructurasdedatos;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class listAdapter extends BaseAdapter {
 
@@ -52,12 +56,41 @@ public class listAdapter extends BaseAdapter {
 
                 TextView Dato = (TextView) vista.findViewById(R.id.textoPerfil);
                 TextView Pos = (TextView) vista.findViewById(R.id.textoFecha);
+                ImageButton Sacar = (ImageButton) vista.findViewById(R.id.unHeapListButton);
 
                 Dato.setTextColor(Color.BLACK);
                 Pos.setTextColor(Color.BLACK);
 
                 Dato.setText(datos[i]);
                 Pos.setText(String.valueOf(i + 1));
+
+                final int usuarioPos = i;
+                Sacar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        new AlertDialog.Builder(contexto)
+                                .setTitle("Sacar usuario")
+                                .setMessage("¿Está seguro de sacar este usuario?")
+
+                                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        //Codigo para sacar a una persona en específico
+                                        //Utilice el usuarioPos para buscar, en el array del listado, la ID del usuario.
+                                    }
+                                })
+
+                                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        return;
+                                    }
+                                })
+                                .setIcon(R.drawable.ic_delete)
+                                .show();
+                    }
+                });
+
+
             }
         }
         return vista;
